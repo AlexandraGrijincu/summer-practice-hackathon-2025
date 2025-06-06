dace483f2517ea71ae88e2b8039383c17b984743
AlexandraGrijincu <alexandra.grijincu05@e-uvt.ro>
2025-06-06 12:43:29 +0300

    2

---
+++ hackathon/myblog/myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse 
+# Create your views here. 
+def home(request): 
+    return render(request, "index.html") 
+def project(request): 
+    return render(request, "project.html") 
+
